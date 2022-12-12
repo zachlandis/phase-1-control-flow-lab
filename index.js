cf2b68e790c7a52d635eb8d0c48d7da98d0d3d02
Zach <zachlandis91@gmail.com>
2022-12-12 11:55:36 -0700

    commit tree 3d8b257cffd44f37dfea88a41847b55895025510

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result;
+  if (someNumber <= 400) {
+    return "This one is on me!"
+  } else if (someNumber > 400 && someNumber < 2000) {
+    return "That will be twenty bucks."
+  } else if (someNumber > 2000 && someNumber < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let location;
+  location = (city === 'NYC') ? "Ok, sounds good." : "No go.";
+  return location;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let reply;
+  
+  switch (tip) {
+    case 'generous':
+      reply = "Thank you so much.";
+      break;
+    case 'not as generous':
+      reply = "Thank you.";
+      break;
+    default:
+      reply = "Bye.";
+  }
+  return reply;
 }
